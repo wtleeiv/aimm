bb6167f1fddedb9d2c02154fe4db500bd21869bd
Tyler Lee <tlee2@illumina.com>
2018-02-06 14:46:20 -0800

    renames

--- ai/projects/search-algs/driver_3.py
+++ ai/projects/search-algs/driver_3.py
@@ -11,34 +11,34 @@ class Board:
     EMPTY_TILE = 0
     POSSIBLE_MOVE = {
         Move.Up: lambda b: b.empty_loc.row > 0,
-        Move.Down: lambda b: b.empty_loc.row < b.board_dim - 1,
+        Move.Down: lambda b: b.empty_loc.row < b.dim - 1,
         Move.Left: lambda b: b.empty_loc.col > 0,
-        Move.Right: lambda b: b.empty_loc.col < b.board_dim - 1
+        Move.Right: lambda b: b.empty_loc.col < b.dim - 1
         }
     def __init__(self, board_str):
         board_arr = list(map(lambda s: int(s), board_str.split(',')))
-        self.board_dim = self.init_board_dim(board_arr)
-        self.layout = self.init_layout(board_arr)
+        self.dim = self.init_dim(board_arr)
+        self.board = self.init_board(board_arr)
         self.solution = self.init_solution()
         self.empty_loc = self.init_empty_loc()
-    def init_board_dim(self, board_arr):
+    def init_dim(self, board_arr):
         dims = modf(sqrt(len(board_arr)))
         assert dims[0] == 0.0, 'board is not square'
         return int(dims[1])
-    def init_layout(self, board_arr):
-        layout = []
+    def init_board(self, board_arr):
+        board = []
         start = 0
-        for end in range(self.board_dim, self.board_dim**2 + 1, self.board_dim):
-            layout.append(board_arr[start:end])
+        for end in range(self.dim, self.dim**2 + 1, self.dim):
+            board.append(board_arr[start:end])
             start = end
-        return layout
+        return board
     def init_solution(self):
-        n = self.board_dim
+        n = self.dim
         return [[j for j in range(n*i, n*(i+1))] for i in range(0,n)]
     def init_empty_loc(self):
-        for row in range(0, self.board_dim):
-            for col in range(0, self.board_dim):
-                if self.layout[row][col] == Board.EMPTY_TILE:
+        for row in range(0, self.dim):
+            for col in range(0, self.dim):
+                if self.board[row][col] == Board.EMPTY_TILE:
                     return Loc(row,col)
         raise Exception('blank tile not present')
     def can_move(self, move):
